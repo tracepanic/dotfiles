e0f1014e743e2492a7e55df3450f650641bce19c
tracepanic <patrickscript@proton.me>
2025-04-04 05:48:23 +0300

    Add prettier formatting on save

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -1,15 +1,20 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    typescript = { "prettier" },
+    javascript = { "prettier" },
+    ["typescriptreact"] = { "prettier" },
+    ["javascriptreact"] = { "prettier" },
+    json = { "prettier" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options
